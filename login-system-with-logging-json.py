ee9f16360da43f0fcd6960a9b9cce24602d834fe
Nguyễn Vinh Phúc <nguyenvinhphuc2007@gmail.com>
2026-05-04 17:22:54 +0700

    Update login-system-with-logging-json.py

--- login-system-with-logging-json.py
+++ login-system-with-logging-json.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import bcrypt
 
 logging.basicConfig(level=logging.INFO, filename="log_users.txt", filemode="a",
                    format="%(asctime)s - %(levelname)s - %(message)s")
@@ -12,70 +13,72 @@ logging.basicConfig(level=logging.INFO, filename="log_users.txt", filemode="a",
 
 def menu():
     while True:
-        print("1. Sign in")
-        print("2. Sign up")
-        print("3. exit")
-        print("4. load (only for developer)")
+        print("1. Log in")
+        print("2. Register")
+        print("3. Exit")
 
-        try:
-            choice = int(input("Choose: "))
-        except ValueError:
-            print("Invalid input")
-            continue
+        choice = input("Choose: ")
 
-        if choice == 1:
-            sign_in()
-        elif choice == 2:
-            sign_up()
-        elif choice == 3:
+        if choice == "1":
+            username = input("Username: ")
+            password = input("Password: ")
+
+            response = log_in(username, password)
+            tries = 0
+            while tries < 3:
+                response = log_in(username, password)
+                print(response)
+
+                if response["status"] == "success":
+                    break
+                else:
+                    tries += 1
+                    print(response)
+
+        elif choice == "2":
+            username = input("Username: ")
+            password = input("Password: ")
+
+            response = register(username, password)
+            print(response)
+        elif choice == "3":
             break
-        elif choice == 4:
-            load()
-        else:
-            print("Invalid")
 
 def save(users):
     with open("users-infos-json.json", "w") as file:
         json.dump(users, file, indent=4)
 
-def sign_up():
+def register(username, password):
     users = load()
-
-    username = input("Enter username: ")
-    password = input("Enter password: ")
 
     if not username or not password:
-        print("Username and password cannot be empty")
-        return
+        return {"status": "error", "message": "empty"}
 
     if username in users:
-        print("Username already existed")
-        return
+        return {"status": "error", "message": "username already registered"}
 
-    users[username] = password
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    users[username] = hashed.decode()
+
     save(users)
-    print("Sign up successful")
     logging.info(f"{username} signed up")
+    return {"status": "success", "user": username}
 
 #status: Done
-def sign_in():
+def log_in(username, password):
     users = load()
-    tries = 0
+    stored_hash = users[username].encode()
 
-    while tries < 3:
-        username = input("Enter username: ")
-        password = input("Enter password: ")
+    if username in users and bcrypt.checkpw(password.encode(), stored_hash):
+        logging.info(f"{username} signed in")
+        return {"status": "success", "user": username}
 
-        if username in users and users[username] == password:
-            print("Login successful")
-            logging.info(f"{username} signed in")
-            return
-        else:
-            print("Login unsuccessful")
-            tries += 1
-    print("Locked")
+    else:
+        return {"status": "error", "message": "login failed"}
+
     logging.warning(f"An intruder tried to log into {username}'s account")
-    return 0
+    return {"status": "warning", "message": "locked"}
+    return
 
 #DON'T TOUCH
 def load():
